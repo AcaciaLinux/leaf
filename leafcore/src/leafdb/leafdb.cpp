995b7df88d7fe5af8161762d73cacbb71c07c50e
Max Kofler <kofler.max.dev@gmail.com>
2022-02-07 17:42:30 +0100

    Moved all of leafdb into one cpp file

--- leafcore/src/leafdb/leafdb.cpp
+++ leafcore/src/leafdb/leafdb.cpp
@@ -29,3 +29,35 @@ bool LeafDB::addPackage(Package* newPackage){
 	
 	return true;
 }
+
+Package* LeafDB::getPackage(std::string name){
+	FUN();
+
+	if (_packages.count(name) == 0)
+		return nullptr;
+
+	return _packages[name];
+}
+
+Package* LeafDB::newPackage(std::string name){
+	LOGD("Adding new package " + name);
+
+	Package* pkg = new Package(name);
+	_packages[name] = pkg;
+	return pkg;
+}
+
+std::vector<Package*> LeafDB::findFileProviders(std::string filepath){
+	FUN();
+	std::vector<Package*> providers;
+
+	for(auto package : _packages){
+		if (package.second == nullptr)
+			continue;
+
+		if (package.second->checkFileProvided(filepath))
+			providers.push_back(package.second);
+	}
+
+	return providers;
+}
