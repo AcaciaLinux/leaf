6dea51b4c16b9a9c5468a2bd6ea6c51086f2f77a
Max Kofler <kofler.max.dev@gmail.com>
2022-03-30 10:08:43 +0200

    Fixed Package test error because of new exception workflow

--- tests/tests/Package/src/tFetch.cpp
+++ tests/tests/Package/src/tFetch.cpp
@@ -12,6 +12,15 @@ TEST(Package, noURL){
 
 	Package* newP = db.newPackage("Test", "1.0");
 
-	if (newP->fetch())
-		FAIL();
+	try{
+		newP->fetch();
+
+		FAIL() << "Did not fail";
+	} catch (LeafError* e){
+		if (e->getErrorCode() != Error::DL_CURL_ERR){
+			FAIL() << "Wrong error thrown";
+		}
+
+		LOGD("Exception OK: " + e->getErrorCodeMessage());
+	}
 }
