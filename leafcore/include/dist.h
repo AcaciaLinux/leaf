b243459dec9245b133ef949cce79f9f3bb974665
Max Kofler <kofler.max.dev@gmail.com>
2022-11-22 19:14:52 +0000

    Added macros to declare leaf version

--- leafcore/include/dist.h
+++ leafcore/include/dist.h
@@ -1,7 +1,15 @@
 #ifndef __DIST_H__
 #define __DIST_H__
 
-#define LEAFCORE_VERSION "0.2.0"
+#define LEAFCORE_V_MAYOR 0
+#define LEAFCORE_V_MINOR 2
+#define LEAFCORE_V_PATCH 1
+
 #define LEAFCORE_CODENAME "Silver Maple"
+
+#define STR_(X) #X
+#define STR(X) STR_(X)
+
+#define LEAFCORE_VERSION STR(LEAFCORE_V_MAYOR) "." STR(LEAFCORE_V_MINOR) "." STR(LEAFCORE_V_PATCH)
 
 #endif
