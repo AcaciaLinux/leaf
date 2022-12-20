a854ec7c2c1d9978f8b3540b6dd44a55e31882d9
Max Kofler <kofler.max.dev@gmail.com>
2022-12-20 20:38:29 +0000

    Added MD5 tests

---
+++ tests/tests/MD5/t_md5.h
@@ -0,0 +1,13 @@
+#ifndef __T_MD5_H__
+#define __T_MD5_H__
+
+#include <gtest/gtest.h>
+#include <stdlib.h>
+
+#include "log.h"
+#include "error.h"
+#include "leafdebug.h"
+#include "checks.h"
+#include "md5.h"
+
+#endif
