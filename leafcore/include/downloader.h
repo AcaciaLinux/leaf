4980e1e75222d6fa9a94b1932e6b2d9ebdf819d9
Max Kofler <kofler.max.dev@gmail.com>
2022-12-20 10:20:16 +0000

    Downloader: Incorporate MD5 hash

--- leafcore/include/downloader.h
+++ leafcore/include/downloader.h
@@ -6,10 +6,22 @@
 #include <string>
 #include <ostream>
 
+#include "md5.h"
+
 class Downloader{
 
 public:
-	Downloader(bool noProgress = false);
+	/**
+	 * @brief	Creates a new Downloader instance
+	 * @param	url			The URL to the download file
+	 * @param	outStream	The output stream
+	 * @param	noProgress	If the downloader should not display any progress
+	 */
+	Downloader(std::string url, std::ostream& outStream, bool noProgress = false);
+
+	/**
+	 * @brief	Cleans up everything
+	 */
 	~Downloader();
 
 	/**
@@ -20,22 +32,49 @@ public:
 
 	/**
 	 * @brief	Downloads from the supplied url to the supplied stream
-	 * @param	url			The URL to download from
-	 * @param	out			The stream to supply the data to
 	 * @param	prefix		The prefix text to the progress bar
 	 * @return	The HTTP response code
 	 */
-	size_t					download(std::string url, std::ostream& out, std::string prefix = "");
+	size_t					download(std::string prefix = "");
+
+	/**
+	 * @brief	Returns the MD5 hash of the downloaded content
+	 */
+	std::string				getMD5() ;
 
 #ifndef FRIEND_DOWNLOADER
 private:
 #endif
 
-	//A curl object to operate with
+	/**
+	 * @brief	The CURL object to work with
+	 */
 	CURL*					_curl = nullptr;
 
-	//Do not display the animated progress bar
+	/**
+	 * @brief	If the animated progress bar should not be displayed
+	 */
 	bool					_noProgress = false;
+
+	/**
+	 * @brief	The stream the downloader outputs its downloaded content to
+	 */
+	std::ostream&			_outStream;
+
+	/**
+	 * @brief	The url to fetch the content from
+	 */
+	std::string				_url;
+
+	/**
+	 * @brief	The md5 hash of the fetched content
+	 */
+	MD5						_md5;
+
+	/**
+	 * @brief	The function that gets called to write data
+	 */
+	static size_t			writeFunc(void* ptr, size_t size, size_t nmemb, std::ostream *s);
 };
 
 #endif
