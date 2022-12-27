174886afd606e1bb2ed1741390f9645cb8a39244
Max Kofler <kofler.max.dev@gmail.com>
2022-12-27 11:13:12 +0000

    Added new Leafcore tests for parseConfig()

--- tests/tests/Leafcore/t_leafcore.h
+++ tests/tests/Leafcore/t_leafcore.h
@@ -1,14 +1,15 @@
-#ifndef __T_PACKAGE_H__
-#define __T_PACKAGE_H__
+#ifndef __T_LEAFCORE_H__
+#define __T_LEAFCORE_H__
 
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
-#define FRIEND_LEAFCORE
-#include "leafdebug.h"
-#include "leafcore.h"
-#include "leafconfig.h"
 #include "log.h"
 #include "error.h"
+#include "leafdebug.h"
+#include "checks.h"
+
+#define FRIEND_LEAFCORE
+#include "leafcore.h"
 
 #endif
