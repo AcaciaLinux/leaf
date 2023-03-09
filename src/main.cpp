5110b85b9fc75fb322ede49cac83edbfc443a99f
Max Kofler <kofler.max.dev@gmail.com>
2023-03-09 09:33:09 +0100

    main: Catch std::exception correctly

--- src/main.cpp
+++ src/main.cpp
@@ -129,8 +129,8 @@ int main(int argc, char** argv){
 			}
 		} catch (LeafError* e){
 			LOGUE("Failed with error code " + std::to_string(e->getErrorCode()) + ": " + e->what());
-		} catch (std::exception* e){
-			LOGUE("Failed with fatal exception: " + std::string(e->what()));
+		} catch (std::exception& e){
+			LOGUE("Failed with fatal exception: " + std::string(e.what()));
 		} catch (...){
 			LOGUE("Failed with unknown fatal exception");
 		}
